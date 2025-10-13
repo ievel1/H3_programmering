3a88d59993d5adc85fec8d9164e6867b455eada2
ievel1 <flekkzero@gmail.com>
2025-10-13 12:10:20 +0200

    H3-Web Api Projekt - Controller - Database First: First commit

--- c#_opgaver/TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ c#_opgaver/TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8412658111ece39d9d77ff0da0f8f91e84381d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
